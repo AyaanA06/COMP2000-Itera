c7ec24ed58abf5ba7442010792d2e7e2806ef6b3
AyaanA06 <ayaan.akhan006@gmail.com>
2026-09-09 12:51:08 +1000

    Extract Medic into separate class

--- Project/Human.java
+++ Project/Human.java
@@ -514,37 +514,3 @@ class Soldier extends Human {
         drawTypeLabel(g, "S");
     }
 }
-
-
-/* MEDIC */
-class Medic extends Human {
-
-    private int medKits = 3;
-
-    public Medic(int x, int y) {
-
-        super(x, y);
-    }
-
-    public void heal(Character target) {
-
-        if (medKits > 0 && target.health < 100) {
-
-            target.health += 20;
-
-            if (target.health > 100) {
-                target.health = 100;
-            }
-
-            medKits--;
-        }
-    }
-
-    @Override
-    public void draw(Graphics g) {
-
-        super.draw(g);
-
-        drawTypeLabel(g, "M");
-    }
-}
---
+++ Project/Medic.java
@@ -0,0 +1,27 @@
+import java.awt.Graphics;
+
+public class Medic extends Human {
+    private int medKits = 3;
+
+    public Medic(int x, int y) {
+        super(x, y);
+    }
+
+    public void heal(Character target) {
+        if (medKits > 0 && target.health < 100) {
+            target.health += 20;
+
+            if (target.health > 100) {
+                target.health = 100;
+            }
+
+            medKits--;
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        super.draw(g);
+        drawTypeLabel(g, "M");
+    }
+}
