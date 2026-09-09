9c7d257b8f7843649fcd6cf02983e3862280100a
AyaanA06 <ayaan.akhan006@gmail.com>
2026-09-09 13:31:11 +1000

    Extract ConvenienceStore into separate class

--- Project/Building.java
+++ Project/Building.java
@@ -99,35 +99,3 @@ public abstract class Building {
 
     protected abstract void drawBuildingBody(Graphics g);
 }
-/* CONVENIENCE STORE */
-class ConvenienceStore extends Building {
-
-    private ArrayList<Food> shelves = new ArrayList<>();
-
-        public ConvenienceStore(int x, int y) {
-
-            super(15, x, y, 180, 180, "CONVENIENCE STORE");
-        
-            Food food = new Food(10, 20);
-        
-            shelves.add(food);
-            stock.add(food);
-        }
-
-    public Food getFood() {
-
-        if (shelves.isEmpty()) {
-            return null;
-        }
-
-        return shelves.remove(0);
-    }
-
-    @Override
-    protected void drawBuildingBody(Graphics g) {
-
-        g.setColor(new Color(255, 245, 200));
-
-        g.fillRect(x, y, width, height);
-    }
-}
---
+++ Project/ConvenienceStore.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class ConvenienceStore extends Building {
+
+    private ArrayList<Food> shelves = new ArrayList<>();
+
+        public ConvenienceStore(int x, int y) {
+
+            super(15, x, y, 180, 180, "CONVENIENCE STORE");
+
+            Food food = new Food(10, 20);
+
+            shelves.add(food);
+            stock.add(food);
+        }
+
+    public Food getFood() {
+
+        if (shelves.isEmpty()) {
+            return null;
+        }
+
+        return shelves.remove(0);
+    }
+
+    @Override
+    protected void drawBuildingBody(Graphics g) {
+
+        g.setColor(new Color(255, 245, 200));
+
+        g.fillRect(x, y, width, height);
+    }
+}
