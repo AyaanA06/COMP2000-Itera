714649bc19a715471537639d68a8e4f8262e6f96
AyaanA06 <ayaan.akhan006@gmail.com>
2026-09-09 13:27:14 +1000

    Extract Food into separate class

---
+++ Project/Food.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Food extends Resource {
+
+    private int nutrition;
+
+    public Food(int quantity, int nutrition) {
+
+        super(quantity);
+
+        this.nutrition = nutrition;
+    }
+
+    public Food(int quantity, int nutrition, int x, int y) {
+
+        super(quantity, x, y);
+
+        this.nutrition = nutrition;
+    }
+
+    @Override
+    public void use(Character target) {
+
+        if (quantity <= 0) {
+            return;
+        }
+
+        if (target instanceof Human human) {
+
+            human.stamina += nutrition;
+
+            if (human.stamina > 100) {
+
+                human.stamina = 100;
+            }
+
+            quantity--;
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+        if (collected) {
+            return;
+        }
+
+        g.setColor(new Color(230, 170, 60));
+
+        g.fillRect(x, y, size, size);
+
+        g.setColor(Color.BLACK);
+
+        g.drawRect(x, y, size, size);
+    }
+}
--- Project/Resource.java
+++ Project/Resource.java
@@ -61,63 +61,6 @@ public abstract class Resource {
 
     public abstract void draw(Graphics g);
 }
-/* FOOD */
-class Food extends Resource {
-
-    private int nutrition;
-
-    public Food(int quantity, int nutrition) {
-
-        super(quantity);
-
-        this.nutrition = nutrition;
-    }
-
-    public Food(int quantity, int nutrition, int x, int y) {
-
-        super(quantity, x, y);
-
-        this.nutrition = nutrition;
-    }
-
-    @Override
-    public void use(Character target) {
-
-        if (quantity <= 0) {
-            return;
-        }
-
-        if (target instanceof Human human) {
-
-            human.stamina += nutrition;
-
-            if (human.stamina > 100) {
-
-                human.stamina = 100;
-            }
-
-            quantity--;
-        }
-    }
-
-    @Override
-    public void draw(Graphics g) {
-
-        if (collected) {
-            return;
-        }
-
-        g.setColor(new Color(230, 170, 60));
-
-        g.fillRect(x, y, size, size);
-
-        g.setColor(Color.BLACK);
-
-        g.drawRect(x, y, size, size);
-    }
-}
-
-
 /* WEAPON */
 class Weapon extends Resource {
 
